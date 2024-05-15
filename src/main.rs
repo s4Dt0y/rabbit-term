e3c0a536b2b5c85ffb6fa4e759b13710e48f3807
s4:D_t0y <roccowu949@gmail.com>
2024-05-15 11:04:25 -0700

    test

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,59 @@
-use nix::pty::openpty;
-use nix::unistd::{close, read};
+use nix::errno::Errno;
+use nix::unistd::ForkResult;
+use std::ffi::CStr;
+use std::os::fd::{AsRawFd, OwnedFd};
 
-use std::{
-    fs::File,
-    io,
-    os::fd::{FromRawFd, IntoRawFd},
-    process::{Command, Stdio},
-};
+fn main() {
+    let fd = spawn_shell();
+    let mut buf = vec![0, 255];
 
-#[derive(Debug)]
-struct Pty {
-    fd: i32,
-}
+    let fd = fd.unwrap();
+    set_nonblock(&fd);
 
-fn main() -> io::Result<()> {
-    let pty = create_pty("/bin/sh");
+    loop {
+        let read_res = nix::unistd::read(fd.as_raw_fd(), &mut buf);
 
-    let f = unsafe { File::from_raw_fd(pty.fd.clone()) };
+        if let Err(Errno::EAGAIN) = read_res {
+            println!("e");
+            continue;
+        }
 
-    let output_bytes = output_from_fd(pty.fd.into_raw_fd()).unwrap();
-    let result = String::from_utf8(output_bytes).unwrap();
-
-    println!("Read: {}", result);
-
-    match close(pty.fd) {
-        Ok(_) => println!("Closed"),
-        Err(_) => panic!("Could not close fd"),
-    };
-
-    Ok(())
-}
-
-fn output_from_fd(fd: i32) -> Option<Vec<u8>> {
-    let mut read_buffer = [0; 65536];
-    let read_result = read(fd, &mut read_buffer);
-    match read_result {
-        Ok(bytes_read) => Some(read_buffer[..bytes_read].to_vec()),
-        Err(_) => None,
+        println!("{}", read_res.unwrap());
     }
 }
 
-fn create_pty(process: &str) -> Pty {
-    let ends = openpty(None, None).expect("Could not open pty");
-    let master = ends.master;
-    let slave = ends.slave;
+fn spawn_shell() -> Option<OwnedFd> {
+    unsafe {
+        let res = nix::pty::forkpty(None, None).unwrap();
 
-    let mut builder = Command::new(process);
-    builder.stdin(unsafe { Stdio::from_raw_fd(slave) });
-    builder.stdout(unsafe { Stdio::from_raw_fd(slave) });
-    builder.stderr(unsafe { Stdio::from_raw_fd(slave) });
-    match builder.spawn() {
-        Ok(_) => {
-            let pty = Pty { fd: master };
-            pty
+        match res.fork_result {
+            ForkResult::Parent { .. } => (),
+            ForkResult::Child => {
+                let shell_name = c"/bin/bash";
+                let args: &[&[u8]] = &[b"bash\0", b"--noprofile\0", b"--norc\0"];
+
+                let args: Vec<&'static CStr> = args
+                    .iter()
+                    .map(|v| {
+                        CStr::from_bytes_with_nul(v).expect("Should always have null terminator")
+                    })
+                    .collect::<Vec<_>>();
+
+                nix::unistd::execvp(shell_name, &args).expect("Could not spawn shell");
+                std::process::exit(1);
+            }
         }
-        Err(e) => {
-            panic!("Failed to create pty: {}", e)
-        }
+        Some(res.master)
     }
+}
+
+fn set_nonblock(fd: &OwnedFd) -> () {
+    let flags = nix::fcntl::fcntl(fd.as_raw_fd(), nix::fcntl::FcntlArg::F_GETFL)
+        .expect("Failed to set F_GETFL");
+    let mut flags = nix::fcntl::OFlag::from_bits(flags & nix::fcntl::OFlag::O_ACCMODE.bits())
+        .expect("Failed to set 0_ACCMOD");
+    flags.set(nix::fcntl::OFlag::O_NONBLOCK, true);
+
+    nix::fcntl::fcntl(fd.as_raw_fd(), nix::fcntl::FcntlArg::F_SETFL(flags))
+        .expect("Failed to SET_FL");
 }
